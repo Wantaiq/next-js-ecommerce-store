bc246629e7b440bf4286e6a0a47fcd58596f67f0
Filip <filipdopa@yahoo.com>
2022-06-07 16:53:16 +0200

    Add user authentication

---
+++ pages/api/users/login.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+import sql from '../../../util/database';
+
+export default async function handleLogin(req, res) {
+  if (req.method !== 'POST') {
+    res.status(400).send('Method not allowed');
+  }
+  const users = await sql`SELECT * from users`;
+  const currentUser = users.find((user) => user.username === req.body.userName);
+  if (currentUser === undefined) {
+    res.status(400).send('Cannot find user.');
+    return;
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, currentUser.pwd)) {
+      res.status(200).send('Login successful');
+    } else {
+      res.status(400).send('Login failed.');
+    }
+  } catch (error) {
+    res.status(500).send();
+  }
+}
